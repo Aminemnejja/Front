0be8326b19d5c040255d21c39b13538521795b5f
Aminemnejja <101280583+Aminemnejja@users.noreply.github.com>
2023-12-07 14:15:11 +0100

    Update Résolution_des_systéme_linéaire.py

--- FrontBack/Résolution_des_systéme_linéaire.py
+++ FrontBack/Résolution_des_systéme_linéaire.py
@@ -29,9 +29,9 @@ def main():
     vecteur_html=f.vecteur_html()
     col1, col2 = st.columns(2)
     with col1:
-        st.components.v1.html(matrice_html,width=300, height=300)
+        st.components.v1.html(matrice_html,width=300, height=300,scrolling=True)
     with col2:
-        st.components.v1.html(vecteur_html,width=300, height=300)
+        st.components.v1.html(vecteur_html,width=300, height=300,scrolling=True)
     if selected_option=="Méthode direct":
         operation = st.selectbox("Choisissez l'opération ", ["Gauss","Gauss_jordan","Décomposition LU","Cholesky"])
          
